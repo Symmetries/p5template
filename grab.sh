ba0c8bb2f628abac8ac9f5c9b23bc0c88553ae4e
Symmetries <lopdie101@gmail.com>
2019-05-25 20:35:01 -0400

    Create grab.sh

---
+++ grab.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/Symmetries/p5template/master/index.html
+wget https://raw.githubusercontent.com/Symmetries/p5template/master/sketch.js
+wget https://raw.githubusercontent.com/Symmetries/p5template/master/style.css
