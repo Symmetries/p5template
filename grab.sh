cf393e4ec3f70759451fe3fe617f0a9cf69ae55e
Symmetries <lopdie101@gmail.com>
2021-03-20 17:46:49 -0400

    Delete grab.sh

--- grab.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/Symmetries/p5template/master/index.html
-wget https://raw.githubusercontent.com/Symmetries/p5template/master/sketch.js
-wget https://raw.githubusercontent.com/Symmetries/p5template/master/style.css
